dc4d17e939c6d3abee6eda8dddab8acb8b4fe4d8
safeermohammed <safeerm@microsoft.com>
2018-05-28 11:05:31 -0700

    took latest with Mo changes

--- src/ResourceManager/Automation/Commands.Automation.Test/ScenarioTests/AutomationScenarioTestsBase.cs
+++ src/ResourceManager/Automation/Commands.Automation.Test/ScenarioTests/AutomationScenarioTestsBase.cs
@@ -12,11 +12,17 @@
 // limitations under the License.
 // ----------------------------------------------------------------------------------
 
+using System;
+using System.Linq;
 using Microsoft.Azure.Commands.Common.Authentication;
 using Microsoft.Azure.Management.Automation;
 using Microsoft.Azure.Test;
 using Microsoft.WindowsAzure.Commands.ScenarioTest;
 using Microsoft.WindowsAzure.Commands.Test.Utilities.Common;
+using Microsoft.Azure.Test.HttpRecorder;
+using System.Reflection;
+using Microsoft.Rest;
+using System.Net.Http;
 
 namespace Microsoft.Azure.Commands.Automation.Test
 {
@@ -38,6 +44,8 @@ namespace Microsoft.Azure.Commands.Automation.Test
 
         protected void RunPowerShellTest(params string[] scripts)
         {
+            const string RootNamespace = "ScenarioTests";
+
             using (UndoContext context = UndoContext.Current)
             {
                 context.Start(TestUtilities.GetCallingClass(2), TestUtilities.GetCurrentMethodName(2));
@@ -46,8 +54,13 @@ namespace Microsoft.Azure.Commands.Automation.Test
 
                 helper.SetupEnvironment(AzureModule.AzureResourceManager);
 
+
+                var psModuleFile = this.GetType().FullName.Contains(RootNamespace) ?
+                    this.GetType().FullName.Split(new[] { RootNamespace }, StringSplitOptions.RemoveEmptyEntries).Last().Replace(".", "\\") :
+                    $"\\{this.GetType().Name}";
+
                 helper.SetupModules(AzureModule.AzureResourceManager,
-                    "ScenarioTests\\" + this.GetType().Name + ".ps1",
+                    $"{RootNamespace}{psModuleFile}.ps1",
                     helper.RMProfileModule,
                     helper.GetRMModulePath(@"AzureRM.Automation.psd1"));
 
@@ -57,7 +70,52 @@ namespace Microsoft.Azure.Commands.Automation.Test
 
         protected AutomationClient GetAutomationManagementClient()
         {
-            return TestBase.GetServiceClient<AutomationClient>(new CSMTestEnvironmentFactory());
+            AutomationClient client;
+            TestEnvironment currentEnvironment = new CSMTestEnvironmentFactory().GetTestEnvironment();
+            var credentials = currentEnvironment.AuthorizationContext.TokenCredentials[TokenAudience.Management];
+
+            HttpMockServer server;
+
+            try
+            {
+                server = HttpMockServer.CreateInstance();
+            }
+            catch (ApplicationException)
+            {
+                // mock server has never been initialized, we will need to initialize it.
+                HttpMockServer.Initialize("TestEnvironment", "InitialCreation");
+                server = HttpMockServer.CreateInstance();
+            }
+
+            if (currentEnvironment.UsesCustomUri())
+            {
+                ConstructorInfo constructor = typeof(AutomationClient).GetConstructor(new Type[] { typeof(Uri), typeof(ServiceClientCredentials), typeof(DelegatingHandler[]) });
+                client = constructor.Invoke(new object[] { currentEnvironment.BaseUri, credentials, new DelegatingHandler[] { server } }) as AutomationClient;
+            }
+            else
+            {
+                ConstructorInfo constructor = typeof(AutomationClient).GetConstructor(new Type[] { typeof(ServiceClientCredentials) });
+                client = constructor.Invoke(new object[] { credentials }) as AutomationClient;
+            }
+
+            PropertyInfo subId = typeof(AutomationClient).GetProperty("SubscriptionId", typeof(string));
+            if (subId != null)
+            {
+                subId.SetValue(client, currentEnvironment.SubscriptionId);
+            }
+
+            if (HttpMockServer.Mode == HttpRecorderMode.Playback)
+            {
+                PropertyInfo initialTimeout = typeof(AutomationClient).GetProperty("LongRunningOperationInitialTimeout", typeof(int));
+                PropertyInfo retryTimeout = typeof(AutomationClient).GetProperty("LongRunningOperationRetryTimeout", typeof(int));
+                if (initialTimeout != null && retryTimeout != null)
+                {
+                    initialTimeout.SetValue(client, 0);
+                    retryTimeout.SetValue(client, 0);
+                }
+            }
+
+            return client;
         }
     }
 }
